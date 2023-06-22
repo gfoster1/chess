da0ee431144cf5a1c80ef9afb9aff3caf0b5ee62
George Foster <georgepfoster@gmail.com>
2023-06-21 21:40:50 -0700

    Remove unnesscessary code

--- src/main/java/com/whitespace/ChessBoard.java
+++ src/main/java/com/whitespace/ChessBoard.java
@@ -1,14 +1,9 @@
 package com.whitespace;
 
 import com.whitespace.board.Move;
-import com.whitespace.board.MoveResult;
-import com.whitespace.board.Position;
 import com.whitespace.board.piece.Piece;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Stream;
 
 public interface ChessBoard {
     void play();
